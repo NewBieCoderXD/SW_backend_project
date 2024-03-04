00b78967f6617eaf6936d5a00f863ad58f5e55dc
NewBieCOderXD <fruity5555@gmail.com>
2024-03-04 10:59:07 +0700

    add checkToken and checkRole to restaurants route

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const Restaurant = require("../models/Restaurant");
 const {getRestaurants,getRestaurant,createRestaurant,updateRestaurant,deleteRestaurant,uploadImage,downloadImage} = require("../controllers/restaurants");
-const { checkTokenIfExists } = require("../middleware/auth");
+const { checkToken,checkRole,checkTokenIfExists } = require("../middleware/auth");
 const { upload } = require("../middleware/upload");
 const router = express.Router();
 
 router.route("/")
     .get(checkTokenIfExists,getRestaurants)
-    .post(createRestaurant);
+    .post(checkToken, checkRole("admin"), createRestaurant);
 router.route("/:id")
     .get(checkTokenIfExists,getRestaurant)
-    .put(updateRestaurant)
-    .delete(deleteRestaurant)
+    .put(checkToken, checkRole("admin"), updateRestaurant)
+    .delete(checkToken, checkRole("admin"), deleteRestaurant)
 router.route("/:id/image")
     .post(upload("image",["image/jpeg","image/png"]),uploadImage)
     .get(downloadImage)
