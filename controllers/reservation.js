b16e28c92d3e8792f845c3660c2768e74d2dd0bc
NewBieCOderXD <fruity5555@gmail.com>
2024-03-04 10:06:02 +0700

    add get one reservation

--- controllers/reservation.js
+++ controllers/reservation.js
@@ -24,6 +24,27 @@ exports.getReservations = async function(req,res,next){
         })
     }
 }
+
+exports.getReservation = async function(req,res,next){
+    try{
+        let filterQuery = {};
+        if(req.user.role!="admin"){
+            filterQuery.reservorId=req.user.id
+        }
+        filterQuery._id=req.params.id
+        let reservation = await Reservation.findOne(filterQuery)
+        res.status(200).json({
+            success:true,
+            data:reservation
+        })
+    }
+    catch(err){
+        console.log(err.stack)
+        res.status(400).json({
+            success:false
+        })
+    }
+}
 exports.addReservation = async function(req,res,next){
     try{
         let {restaurantId,reservationPeriod,reservationDate,restaurantName} = req.body;
--- routes/reservation.js
+++ routes/reservation.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { checkToken, checkRole } = require("../middleware/auth");
-const { getReservations, addReservation, updateReservation, deleteReservation } = require("../controllers/reservation");
+const { getReservations, getReservation, addReservation, updateReservation, deleteReservation } = require("../controllers/reservation");
 const router = express.Router();
 
 router.route("/")
     .get(checkToken,checkRole("user","admin"),getReservations)
     .post(checkToken,addReservation)
 router.route("/:id")
+    .get(checkToken,getReservation)
     .put(checkToken,checkRole("user","admin"),updateReservation)
     .delete(checkToken,checkRole("user","admin"),deleteReservation)
 module.exports=router
